b6f1b2fdf91e1a8125b5a5e0d303423107708e2a
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-07-14 12:09:40 +0500

    add App Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import NavBar from "./components/navBar";
+import Home from "./components/home/home";
+import ProductDetails from "./components/productsdetails/productDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./components/notFound/notFound";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path=":slug" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
